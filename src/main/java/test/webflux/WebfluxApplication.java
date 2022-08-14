92c92490bd62d4d8478f53c329c0a25aec0b6779
hyeonsoo-park <hyeonsoo-park@navercorps.com>
2022-08-14 11:58:27 +0900

    add custom client library

--- src/main/java/test/webflux/WebfluxApplication.java
+++ src/main/java/test/webflux/WebfluxApplication.java
@@ -8,6 +8,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class WebfluxApplication {
 
 	public static void main(String[] args) {
+//		System.setProperty("reactor.netty.ioWorkerCount", "4");
 		SpringApplication.run(WebfluxApplication.class, args);
 	}
 }
--- src/main/java/test/webflux/controller/TestController.java
+++ src/main/java/test/webflux/controller/TestController.java
@@ -1,7 +1,13 @@
 package test.webflux.controller;
 
 import lombok.AllArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.context.annotation.Bean;
+import org.springframework.http.client.reactive.ClientHttpConnector;
 import org.springframework.http.client.reactive.ReactorClientHttpConnector;
+import org.springframework.http.client.reactive.ReactorResourceFactory;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.reactive.function.client.WebClient;
@@ -13,19 +19,35 @@ import reactor.netty.tcp.TcpClient;
 import javax.annotation.PostConstruct;
 import java.time.Duration;
 import java.util.List;
+import java.util.function.Function;
 
+@Slf4j
 @RestController
 public class TestController {
     private final WebClient webClient = WebClient.builder()
+            .clientConnector(new ReactorClientHttpConnector(httpClient()))
             .baseUrl("http://localhost:7777")
             .build();
 
+//    private static final Logger logger = LoggerFactory.getLogger(TestController.class);
+
+
     @GetMapping("/api")
     public Mono<List> test() {
-
         return webClient.get().uri("/test")
                 .retrieve()
                 .bodyToMono(List.class);
+    }
+
+    @Bean
+    public HttpClient httpClient() {
+        ConnectionProvider provider = ConnectionProvider.builder("provider")
+                .maxConnections(500)
+                .pendingAcquireMaxCount(-1)
+                .build();
+
+        HttpClient httpClient = HttpClient.create(provider);
+        return httpClient;
     }
 
 }
